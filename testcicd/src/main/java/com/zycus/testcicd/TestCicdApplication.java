764532eb540ececa9a6cfc91387068c0ed5ef1cb
prajjwala <prajjwal.agrawal@zycus.com>
2019-04-15 19:11:54 +0530

    Added Code

---
+++ testcicd/src/main/java/com/zycus/testcicd/TestCicdApplication.java
@@ -0,0 +1,14 @@
+package com.zycus.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCicdApplication.class, args);
+		
+	}
+
+}
